a98fd17a8691c859e9e12e8572609ed84588821e
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-01-10 18:10:22 +0100

    added custom drawing libary

--- Assets/Scripts/RobotScript.cs
+++ Assets/Scripts/RobotScript.cs
@@ -17,14 +17,15 @@ public class RobotScript : MonoBehaviour
     public Material defaultMaterial;
     public Material foundGoalMaterial;
     public MeshRenderer mainBodyRenderer;
-    
+
     public Vector3 nextPos;
     public bool nextPosSet;
-    
+
     private static List<RobotScript> robots = new List<RobotScript>();
     public static int robot_count = 0;
 
     public Material lineRendererMaterial;
+
     void Start()
     {
         //find rb component
@@ -46,14 +47,14 @@ public class RobotScript : MonoBehaviour
         goal = GameObject.Find("Goal");
         goalPosition = Vector3.negativeInfinity;
         foundGoal = false;
-        
+
         //initialize nextPos
         nextPos = Vector3.zero;
         nextPosSet = false;
-        
+
         //set default material
         mainBodyRenderer.material = defaultMaterial;
-        
+
         //disable auto physics
         Physics.autoSimulation = true;
     }
@@ -73,22 +74,19 @@ public class RobotScript : MonoBehaviour
             //rb.MovePosition(transform.position + dirVec);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dirVec), 45f);
             moving = true;
-        } else
+        }
+        else
         {
             transform.position = new Vector3(targetLocation.x, rb.position.y, targetLocation.z);
             moving = false;
         }
 
-        //show debug
-        if (ControllerScript.ctrlScript.debug)
-            ShowDebug();
-
         //test if goal was found
-        if (!foundGoal && Vector3.Distance(goal.transform.position, transform.position) < ControllerScript.ctrlScript.visionRange)
+        if (!foundGoal && Vector3.Distance(goal.transform.position, transform.position) <
+            ControllerScript.ctrlScript.visionRange)
         {
             GoalFound(goal.transform.position);
-        } 
-        
+        }
     }
 
     private void LateUpdate()
@@ -98,7 +96,7 @@ public class RobotScript : MonoBehaviour
         {
             transform.position = nextPos;
         }
-        
+
         // check if robot is out of bounds!
         Vector3 oldpos = transform.position;
         if (Math.Abs(transform.position.x) > 24)
@@ -109,6 +107,10 @@ public class RobotScript : MonoBehaviour
         {
             transform.position = new Vector3(oldpos.x, oldpos.y, Math.Sign(transform.position.z) * 24);
         }
+        
+        //show debug
+        if (ControllerScript.ctrlScript.debug)
+            ShowDebug();
     }
 
     public void GoalFound(Vector3 pos)
@@ -139,23 +141,34 @@ public class RobotScript : MonoBehaviour
         List<RobotScript> robotsInRange = new List<RobotScript>();
         foreach (RobotScript rs in robots)
         {
-            if (rs != this && Vector3.Distance(this.rb.position, rs.rb.position) < ControllerScript.ctrlScript.communicateRange)
+            if (rs != this && Vector3.Distance(this.rb.position, rs.rb.position) <
+                ControllerScript.ctrlScript.communicateRange)
             {
                 robotsInRange.Add(rs);
             }
         }
+
         return robotsInRange;
     }
 
     private void ShowDebug()
     {
-        foreach (var rs in FindRobots())
+        /*foreach (var rs in FindRobots())
             Debug.DrawLine(rb.position, rs.rb.position, new Color(0.5f, 0.2f, 0.2f, 0.5f));
 
         tDebug.DrawEllipse(rb.position, Vector3.up, Vector3.forward, ControllerScript.ctrlScript.communicateRange, ControllerScript.ctrlScript.communicateRange, 32, new Color(0.5f, 0.2f, 0.2f, 1f));
         tDebug.DrawEllipse(rb.position, Vector3.up, Vector3.forward, ControllerScript.ctrlScript.visionRange, ControllerScript.ctrlScript.visionRange, 32, new Color(0.2f, 0.4f, 1f, 1f));
 
         Debug.DrawLine(rb.position, new Vector3(targetLocation.x, transform.position.y, targetLocation.z), new Color(0.2f, 0.4f, 1f, 1f));
+    */
+        if (!foundGoal)
+        {
+            TobiDraw.tobiDraw.DrawLine(
+                transform.position,
+                new Vector3(targetLocation.x, transform.position.y, targetLocation.z),
+                new Color(0.2f, 0.4f, 1f, 1f),
+                1f);
+        }
     }
 
     public void DestroyRobot()
---
+++ Assets/Scripts/TobiDraw.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+/*
+ * This class can draw lines/circles using LineRenderer Components in Game/Scene-View
+ * It uses an object pool to help performance.
+ *
+ * How to use:
+ * 1. Create an Object and add TobiDraw.cs as component
+ * 2. Set lineMaterial
+ * 3. draw stuff using TobiDraw.tobiDraw.DrawX();
+ */
+public class TobiDraw : MonoBehaviour
+{
+    private Stack<LineRenderer> inactiveLineRenderer;
+    private Stack<LineRenderer> activeLineRenderer;
+    public static TobiDraw tobiDraw;
+    public Material lineMaterial;
+    private void Start()
+    {
+        inactiveLineRenderer = new Stack<LineRenderer>();
+        activeLineRenderer = new Stack<LineRenderer>();
+        tobiDraw = this;
+    }
+
+    public void DrawLine(Vector3 a, Vector3 b, Color c, float width = 1f)
+    {
+        var l = GetNewLineObject();
+        l.positionCount = 2;
+        l.SetPosition(0, a);
+        l.SetPosition(1, b);
+        l.startColor = l.endColor = c;
+        l.startWidth = l.endWidth = width;
+    }
+    
+    private void Update()
+    {
+        // reset old lines
+        var n = activeLineRenderer.Count;
+        for (var i = 0; i < n; i++)
+        {
+            var l = activeLineRenderer.Pop();
+            inactiveLineRenderer.Push(l);
+            l.gameObject.SetActive(false);
+        }
+    }
+
+    public LineRenderer GetNewLineObject()
+    {
+        if (inactiveLineRenderer.Count > 0)
+        {
+            // reuse existing LineRenderer-Object
+            var l = inactiveLineRenderer.Pop();
+            activeLineRenderer.Push(l);
+            l.gameObject.SetActive(true);
+            return l;
+        }
+        else
+        {
+            // create new LineRenderer-Object
+            var obj = new GameObject("LineRendererObject");
+            obj.transform.parent = gameObject.transform;
+            var l = obj.AddComponent<LineRenderer>();
+            activeLineRenderer.Push(l);
+            return l;
+        }
+    }
+}
